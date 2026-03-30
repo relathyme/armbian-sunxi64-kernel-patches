0c6d86ae5d4bb7744b3aceb87ecd931a16406bd6
relathyme <relathyme@protonmail.com>
2026-03-30 21:31:21 +0700

    update uboot script and env

--- misc/boot/loader/boot.cmd
+++ misc/boot/loader/boot.cmd
@@ -4,11 +4,16 @@ setenv boot_device ${devtype} ${devnum}
 load ${boot_device} ${boot_tmp_addr} /env.txt
 env import -t ${boot_tmp_addr} ${filesize}
 
-load ${boot_device} ${kernel_addr_r} ${boot_loader}
+if itest.s ${boot_spi} == "true"; then
+	sf probe 0
+	sf read ${boot_tmp_addr} 0x000f0000 0x00010000
+	env import -d ${boot_tmp_addr}
+	run bootcmd
+fi
+
 load ${boot_device} ${fdt_addr_r} ${boot_dtb}
 fdt addr ${fdt_addr_r}
 fdt resize 65536
-
 for overlay in ${overlays}; do
 	if load ${boot_device} ${boot_tmp_addr} /dtb/overlay/${overlay}.dtbo; then
 		echo "applying overlay: ${overlay}"
@@ -16,4 +21,5 @@ for overlay in ${overlays}; do
 	fi
 done
 
-bootefi ${kernel_addr_r} ${fdt_addr_r}
+load ${boot_device} ${boot_tmp_addr} ${boot_kernel}
+bootm ${boot_tmp_addr} ${boot_tmp_addr} ${fdt_addr_r}
